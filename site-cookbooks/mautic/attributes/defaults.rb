f58702b658e20c713fb55205156b2209420af0ea
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-21 12:31:01 +0900

    1.3.1

--- site-cookbooks/mautic/attributes/defaults.rb
+++ site-cookbooks/mautic/attributes/defaults.rb
@@ -3,8 +3,8 @@ default[:lw1_mautic][:group] = 'apache'
 
 ## Sum = Sha256
 default[:lw1_mautic][:install] = {
-  version: '1.3.0',
-  checksum: "2304437143043a999d26e2dcd7d8d1464621bfce6a1703d27ebdec5502e503cc",
+  version: '1.3.1',
+  checksum: "5095cbd8212dc826efb3e0008027e95d4d4a0ded10886a922f5ff8a62955c0c3",
   base_url: "https://github.com/mautic/mautic/archive/"
 }
 
