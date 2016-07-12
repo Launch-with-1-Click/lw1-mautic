1f6472fcc057554dbe8849182570439f6ebf4a6e
sawanoboly <sawanoboriyu@higanworks.com>
2016-07-12 14:11:01 +0900

    milestone/#1

--- site-cookbooks/mautic/recipes/pre_packages.rb
+++ site-cookbooks/mautic/recipes/pre_packages.rb
@@ -29,7 +29,8 @@ ruby_block "update php.ini" do
   block do
     _file = Chef::Util::FileEdit.new('/etc/php.ini')
     _file.search_file_replace_line(/^post_max_size/, "post_max_size = 258M ;")
-    _file.search_file_replace_line(/^lesize/, "upload_max_filesize = 256M ;")
+    _file.search_file_replace_line(/^upload_max_filesize/, "upload_max_filesize = 256M ;")
+    _file.search_file_replace_line(/^max_execution_time/, "max_execution_time = 600 ;")
     _file.search_file_replace_line(/^max_execution_time/, "max_execution_time = 600 ;")
     _file.write_file
   end
