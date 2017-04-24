5636470492b35e0b986c45f872d90b54acecc65a
sawanoboly <sawanoboriyu@higanworks.com>
2017-04-24 17:23:47 +0900

    install 2.6.1 by default

--- site-cookbooks/mautic/attributes/defaults.rb
+++ site-cookbooks/mautic/attributes/defaults.rb
@@ -3,8 +3,8 @@ default[:lw1_mautic][:group] = 'apache'
 
 ## Sum = Sha256
 default[:lw1_mautic][:install] = {
-  version: '2.7.1',
-  checksum: "216c46db975ba039c7d301296c8e6ad4d1036fe598219840ab7f70fc52f3bd7c",
+  version: '2.6.1',
+  checksum: "364c3ea010f77a4b44ac1ab472a6fc7e6074980e7ab4d036dd6aa54c96ecca29",
   base_url: "https://github.com/mautic/mautic/archive/"
 }
 
